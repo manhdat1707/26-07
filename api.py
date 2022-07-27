5f91cb74a1a8d59b0369376303bb9d089eff2e0e
manhdat1707 <109257253+manhdat1707@users.noreply.github.com>
2022-07-27 14:25:29 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-
 from config import *
 
 
@@ -8,7 +7,8 @@ def get_User():
     results = []
     if r.exists('user'):
         results = json.loads(r.get('user'))
-    else :
+    else 
+    
         data = User.get_list_user()
         rval = json.dumps(data)
         r.set('user' ,rval)
@@ -16,19 +16,17 @@ def get_User():
     return jsonify({
         "data": results
     })
-
-
-
-    
-        
-
-
-
-        
-
-    
+@app.route('/update/<int:id>', methods=['PUT'])
+def update_User(id):
+    request_data = request.get_json()
+    User.update_one_user(id ,request_data['name'], request_data['age'])
+    r.delete("user")
+    results = list(User.select().where(User.id == id).dicts())
+    return jsonify({
+        "code": 0,
+        "data": results[0]
+    }), 200
 
     
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
-
